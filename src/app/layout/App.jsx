b2fe02221ab4d60dfb89da68f6cf1b3865847ed2
MAZHAR <mazharchdev@gmail.com>
2018-09-16 14:37:41 -0700

    Home Page Completed

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import EventDashboard from "../../features/event/eventDashBoard/EventDashBoard.jsx";
 import NavBar from "../../features/nav/navbar/NavBar";
 import { Container } from "semantic-ui-react";
-import { Route } from 'react-router-dom';
+import { Route,Switch } from 'react-router-dom';
 import EventDetailedPage  from '../../features/event/eventDetailed/EventDetailedPage';
 import PeopleDashBoard from '../../features/user/PeopleDashboard/PeopleDashboard'
 import UserDetails from '../../features/user/userDetails/UserDetails'
@@ -13,18 +13,30 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Switch>
+        <Route exact path="/" component={HomePage} />   
+        </Switch>
+    
+    <Route
+    path="/(.+)"
+    render={()=>(
+      <div>
         <NavBar />
         <Container className="main">
-        <Route exact path="/" component={HomePage} />
+        <Switch>
         <Route path="/events" component={EventDashboard} />
         <Route path="/event/:id" component={EventDetailedPage} />
         <Route path="/people" component={PeopleDashBoard} />
         <Route path="/profile/:id" component={UserDetails} />
         <Route path="/settings" component={SettingsDashboard} />
         <Route path="/createEvent" component={EventForm} />
-           
+        </Switch>
         </Container>
       </div>
+    )}
+    />
+
+      </div>      
     );
   }
 }
