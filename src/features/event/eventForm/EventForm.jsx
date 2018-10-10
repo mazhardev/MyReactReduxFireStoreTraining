bbf6282f8099ddaa14303388f16e6500f19069b2
MAZHAR <mazharchdev@gmail.com>
2018-10-10 12:37:53 -0700

    Form submission in Redux forms

--- src/features/event/eventForm/EventForm.jsx
+++ src/features/event/eventForm/EventForm.jsx
@@ -10,18 +10,12 @@ import SelectInput from "../../../app/common/form/SelectInput";
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
-  let event = {
-    title: "",
-    date: "",
-    city: "",
-    venue: "",
-    hostedBy: ""
-  };
+  let event = { };
   if (eventId && state.events.length > 0) {
     event = state.events.filter(event => event.id === eventId)[0];
   }
   return {
-    event
+    initialValues:event
   };
 };
 const actions = {
@@ -38,19 +32,20 @@ const category = [
 ];
 class EventForm extends Component {
  
-  onFormSubmit = evt => {
-    evt.preventDefault();
-    if (this.state.event.id) {
-      this.props.updateEvent(this.state.event);
-      this.props.history.goBack();
-    } else {
+  onFormSubmit = values => {
+   
+     if (this.props.initialValues.id) {
+       this.props.updateEvent(values);
+       this.props.history.goBack();
+     } else {
       const newEvent = {
-        ...this.state.event,
-        id: cuid(),
-        hostPhotoURL: "/assets/user.png"
-      };
-      this.props.createEvent(newEvent);
-      this.props.history.push("/events");
+         ...values,
+         id: cuid(),
+         hostPhotoURL: "/assets/user.png",
+         hostedBy:"Mazhar"
+       };
+       this.props.createEvent(newEvent);
+       this.props.history.push("/events");
     }
   };
   render() {
@@ -58,7 +53,7 @@ class EventForm extends Component {
       <Grid>
         <Grid.Column width={10}>
           <Segment>
-            <Form onSubmit={this.onFormSubmit}>
+            <Form onSubmit={this.props.handleSubmit(this.onFormSubmit)}>
               <Header sub color="teal" content="Event Details" />
               <Field
                 name="title"
@@ -116,4 +111,4 @@ class EventForm extends Component {
 export default connect(
   mapState,
   actions
-)(reduxForm({ form: "EventForm" })(EventForm));
+)(reduxForm({ form: "EventForm" ,enableReinitialize:true })(EventForm));
