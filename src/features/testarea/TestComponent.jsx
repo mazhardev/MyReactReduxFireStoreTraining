8be6948e7e9875b01b59c2cca103bc44d0a0051a
MAZHAR <mazharchdev@gmail.com>
2018-10-12 12:29:51 -0700

    Setting up Places Autocomplete

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Button } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { incrementCounter, decrementCounter } from './actions'
+import  Script  from "react-load-script"
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 
 const mapState = (state) => ({
   data: state.test.data
@@ -13,14 +15,43 @@ const actions = {
 }
 
 class TestComponent extends Component {
+
+  state={
+    address:""  ,
+    scriptLoaded:false
+  }
+  handleScriptLoad=()=>{this.setState({scriptLoaded:true})} 
+  handleFormSubmit = (event) => {
+    event.preventDefault()
+    geocodeByAddress(this.state.address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error))
+  }
+onChange=(address)=>{this.setState({address})}
+
   render() {
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange,
+    }
     const {incrementCounter, decrementCounter, data} = this.props;
     return (
       <div>
+        <Script
+        url='https://maps.googleapis.com/maps/api/js?key=AIzaSyAqe2Hx0D8PX6MdqaIXKQvxr_ysHwj-I04&libraries=places'
+        onLoad={this.handleScriptLoad}
+       />
         <h1>Test Area</h1>
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} color='green' content='Increment' />
         <Button onClick={decrementCounter} color='red' content='Decrement' />
+        <br/><br/>
+        <form onSubmit={this.handleFormSubmit}>
+        {this.state.scriptLoaded &&  <PlacesAutocomplete inputProps={inputProps} /> }
+       
+        <button type="submit">Submit</button>
+      </form>
       </div>
     )
   }
