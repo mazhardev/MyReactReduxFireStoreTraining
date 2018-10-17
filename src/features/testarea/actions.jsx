fa80277a629129c6c98496805f6c4142e82aa2c6
MAZHAR <mazharchdev@gmail.com>
2018-10-17 04:43:23 -0700

    Redux thunk create constatnts , Actions

--- src/features/testarea/actions.jsx
+++ src/features/testarea/actions.jsx
@@ -1,4 +1,4 @@
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from './testContans';
+import { INCREMENT_COUNTER, DECREMENT_COUNTER,COUNTER_ACTION_STARTED,COUNTER_ACTION_FINISHED } from './testContans';
 
 export const incrementCounter = () => {
   return {
@@ -10,4 +10,33 @@ export const decrementCounter = () => {
   return {
     type: DECREMENT_COUNTER
   }
+}
+export const startedCounterAction=()=>{
+  return{
+    type:COUNTER_ACTION_STARTED
+  }
+}
+export const finishCounterAction=()=>{
+  return{
+    type:COUNTER_ACTION_FINISHED
+  }
+}
+const delay=(ms)=>{
+  return new Promise(resolve=>setTimeout(resolve,ms))
+}
+export const incrementAsync=()=>{
+   return async dispatch=>{
+     dispatch(startedCounterAction())
+     await delay(1000);
+     dispatch({type:INCREMENT_COUNTER})
+     dispatch(finishCounterAction())
+   }
+}
+export const decrementAsync=()=>{
+   return async dispatch=>{
+     dispatch(startedCounterAction())
+     await delay(1000);
+     dispatch({type:DECREMENT_COUNTER})
+     dispatch(finishCounterAction())
+   }
 }
