cc1c5a3ab405ab5a1c719143deace6f4be087687
MAZHAR <mazharchdev@gmail.com>
2018-10-18 15:37:14 -0700

    Firestore 4.13.0 update to date objects

--- src/features/event/eventList/EventListItem.jsx
+++ src/features/event/eventList/EventListItem.jsx
@@ -25,16 +25,16 @@ class EventListItem extends Component {
           <Segment>
             <span>
               <Icon name="clock" />
-              {format(event.date, "dddd Do MMMM")} at{" "}
-              {format(event.date, "HH:mm")} |<Icon name="marker" />{" "}
+              {format(event.date.toDate(), "dddd Do MMMM")} at{" "}
+              {format(event.date.toDate(), "HH:mm")} |<Icon name="marker" />{" "}
               {event.venue}
             </span>
           </Segment>
           <Segment secondary>
             <List horizontal>
               {event.attendees &&
-               Object.values(event.attendees).map(attendee => (
-                  <EventListAttendee key={attendee.id} attendee={attendee} />
+               Object.values(event.attendees).map((attendee,index) => (
+                  <EventListAttendee key={index} attendee={attendee} />
                 ))}
             </List>
           </Segment>
