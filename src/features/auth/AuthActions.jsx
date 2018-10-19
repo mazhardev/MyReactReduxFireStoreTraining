1933da65c542ef6d8c1d09f7e64c38c30209bb7f
MAZHAR <mazharchdev@gmail.com>
2018-10-19 15:14:58 -0700

    Creating the register method

--- src/features/auth/AuthActions.jsx
+++ src/features/auth/AuthActions.jsx
@@ -1,6 +1,6 @@
-import { SIGN_OUT_USER } from './AuthConstants'
+import { SIGN_OUT_USER } from "./AuthConstants";
 import { closeModal } from "../modals/modalActions";
-import {SubmissionError} from "redux-form"
+import { SubmissionError } from "redux-form";
 
 export const login = creds => {
   return async (dispatch, getState, { getFirebase }) => {
@@ -13,8 +13,8 @@ export const login = creds => {
     } catch (error) {
       console.log(error);
       throw new SubmissionError({
-        _error:"Login failed"
-      })
+        _error: "Login failed"
+      });
     }
   };
 };
@@ -23,4 +23,32 @@ export const logout = () => {
   return {
     type: SIGN_OUT_USER
   };
+};
+export const registerUser = user => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  try {
+    //create user in auth
+    let createUser = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(user.email, user.password);
+    console.log(createUser);
+    //update the auth profile
+    await createUser.updateProfile({
+      displayName: user.displayName
+    });
+    //create a new profie in firestore
+    let newUser = {
+      displayName: user.displayName,
+      createdAt: firestore.FieldValue.serverTimestamp()
+    };
+    await firestore.set(`users/${createUser.uid}`, { ...newUser });
+    dispatch(closeModal());
+  } catch (error) {
+    console.log(error);
+  }
 };
