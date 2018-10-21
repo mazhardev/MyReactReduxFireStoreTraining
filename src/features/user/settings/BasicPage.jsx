373be7880f8070f0eae7c029b72cb1b450b71f00
MAZHAR <mazharchdev@gmail.com>
2018-10-21 13:21:55 -0700

    Creating a reusable Radio input field

--- src/features/user/settings/BasicPage.jsx
+++ src/features/user/settings/BasicPage.jsx
@@ -1,48 +1,75 @@
-import React, {Component} from 'react';
-import {Segment, Form, Header, Divider, Button} from 'semantic-ui-react';
-import {Field, reduxForm} from 'redux-form';
+import React, { Component } from "react";
+import { Segment, Form, Header, Divider, Button } from "semantic-ui-react";
+import { Field, reduxForm } from "redux-form";
 import DateInput from "../../../app/common/form/DateInput";
 import PlacesInput from "../../../app/common/form/PlacesInput";
 import TextInput from "../../../app/common/form/TextInput";
+import RadioInput from "../../../app/common/form/RadioInput";
+import moment from "moment";
 
 class BasicPage extends Component {
-
-    render() {
-        const {pristine, submitting} = this.props;
-        return (
-            <Segment>
-                <Header dividing size='large' content='Basics' />
-                <Form>
-                    <Field
-                        width={8}
-                        name='displayName'
-                        type='text'
-                        component={TextInput}
-                        placeholder='Known As'
-                    />
-                    <Form.Group inline>
-                      {/* todo: Gender Radio button */}
-                    </Form.Group>
-                    <Field
-                        width={8}
-                        name='dateOfBirth'
-                        component={DateInput}
-                        placeholder='Date of Birth'
-                    />
-                    <Field
-                        name='city'
-                        placeholder='Home Town'
-                        options={{types: ['(cities)']}}
-                        label='Female'
-                        component={PlacesInput}
-                        width={8}
-                    />
-                    <Divider/>
-                    <Button disabled={pristine || submitting} size='large' positive content='Update Profile'/>
-                </Form>
-            </Segment>
-        );
-    }
+  render() {
+    const { pristine, submitting } = this.props;
+    return (
+      <Segment>
+        <Header dividing size="large" content="Basics" />
+        <Form>
+          <Field
+            width={8}
+            name="displayName"
+            type="text"
+            component={TextInput}
+            placeholder="Known As"
+          />
+          <Form.Group inline>
+            <label>Gender</label>
+            <Field
+              name="gender"
+              type="radio"
+              value="male"
+              label="Male"
+              component={RadioInput}
+            />
+            <Field
+              name="gender"
+              type="radio"
+              value="female"
+              label="Female"
+              component={RadioInput}
+            />
+          </Form.Group>
+          <Field
+            width={8}
+            name="dateOfBirth"
+            component={DateInput}
+            placeholder="Date of Birth"
+            dateFormat="YYYY-MM-DD"
+            showYearDropdown={true}
+            showMonthDropdown={true}
+            dropdownMode="select"
+            maxDate={moment().subtract(18, "years")}
+          />
+          <Field
+            name="city"
+            placeholder="Home Town"
+            options={{ types: ["(cities)"] }}
+            label="Female"
+            component={PlacesInput}
+            width={8}
+          />
+          <Divider />
+          <Button
+            disabled={pristine || submitting}
+            size="large"
+            positive
+            content="Update Profile"
+          />
+        </Form>
+      </Segment>
+    );
+  }
 }
 
-export default reduxForm({form: 'userProfile',enableReinitialize:true})(BasicPage);
+export default reduxForm({ form: "userProfile", enableReinitialize: true })(
+  BasicPage
+);
