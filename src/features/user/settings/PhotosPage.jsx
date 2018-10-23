20244c8acfbb6f07a866b2bbacb0e4310f00d46f
MAZHAR <mazharchdev@gmail.com>
2018-10-23 16:09:07 -0700

    Displaying the photos on the page

--- src/features/user/settings/PhotosPage.jsx
+++ src/features/user/settings/PhotosPage.jsx
@@ -18,18 +18,19 @@ import { toastr } from "react-redux-toastr";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 
-const mapState=(state)=>({
+const mapState = state => ({
   auth: state.firebase.auth,
   profile: state.firebase.profile,
-})
+  photos: state.firestore.ordered.photos
+});
 
 const query = ({ auth }) => {
   return [
     {
-      collection: 'users',
+      collection: "users",
       doc: auth.uid,
-      subcollections: [{ collection: 'photos' }],
-      storeAs: 'photos'
+      subcollections: [{ collection: "photos" }],
+      storeAs: "photos"
     }
   ];
 };
@@ -83,6 +84,13 @@ class PhotosPage extends Component {
     });
   };
   render() {
+    const { photos, profile } = this.props;
+    let filteredPhotos;
+    if (photos) {
+      filteredPhotos = photos.filter(photo => {
+        return photo.url !== profile.photoURL;
+      });
+    }
     return (
       <Segment>
         <Header dividing size="large" content="Your Photos" />
@@ -148,19 +156,21 @@ class PhotosPage extends Component {
 
         <Card.Group itemsPerRow={5}>
           <Card>
-            <Image src="https://randomuser.me/api/portraits/men/20.jpg" />
+            <Image src={profile.photoURL} />
             <Button positive>Main Photo</Button>
           </Card>
-
-          <Card>
-            <Image src="https://randomuser.me/api/portraits/men/20.jpg" />
-            <div className="ui two buttons">
-              <Button basic color="green">
-                Main
-              </Button>
-              <Button basic icon="trash" color="red" />
-            </div>
-          </Card>
+          {photos &&
+            filteredPhotos.map(photo => (
+              <Card key={photo.id}>
+                <Image src={photo.url} />
+                <div className="ui two buttons">
+                  <Button basic color="green">
+                    Main
+                  </Button>
+                  <Button basic icon="trash" color="red" />
+                </div>
+              </Card>
+            ))}
         </Card.Group>
       </Segment>
     );
