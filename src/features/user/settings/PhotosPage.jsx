f00a3f86e74724c9625d41788467f2b159242768
MAZHAR <mazharchdev@gmail.com>
2018-10-23 12:37:55 -0700

    React Dropzone

--- src/features/user/settings/PhotosPage.jsx
+++ src/features/user/settings/PhotosPage.jsx
@@ -1,7 +1,20 @@
 import React, {Component} from 'react';
-import {Image, Segment, Header, Divider, Grid, Button, Card} from 'semantic-ui-react';
-
+import {Image, Segment, Header, Divider, Grid, Button, Card,Icon} from 'semantic-ui-react';
+import Dropzone from "react-dropzone"
+import {connect} from "react-redux"
 class PhotosPage extends Component {
+
+    state={
+      files:[],
+      fileName:''
+    }
+    onDrop=(files)=>{
+      this.setState({
+        files,
+        fileName:files[0].name
+      })
+    }
+
     render() {
         return (
             <Segment>
@@ -10,14 +23,23 @@ class PhotosPage extends Component {
                     <Grid.Row />
                     <Grid.Column width={4}>
                         <Header color='teal' sub content='Step 1 - Add Photo'/>
+                        <Dropzone onDrop={this.onDrop} multiple={false} >
+                          <div style={{paddingTop:"30px" , textAlign:"center"}}>
+                            <Icon name='upload' size='huge' />
+                            <Header content="Drop image here or click to add" />
+                          </div>
+                        </Dropzone>
                     </Grid.Column>
                     <Grid.Column width={1} />
                     <Grid.Column width={4}>
                         <Header sub color='teal' content='Step 2 - Resize image' />
-                    </Grid.Column>
+                                 </Grid.Column>
                     <Grid.Column width={1} />
                     <Grid.Column width={4}>
                         <Header sub color='teal' content='Step 3 - Preview and Upload' />
+                        {this.state.files[0] &&
+                        <Image style={{minHeight:"200px" , minWidth:"200px"}} src={this.state.files[0].preview} />}
+    
                     </Grid.Column>
 
                 </Grid>
@@ -46,4 +68,4 @@ class PhotosPage extends Component {
     }
 }
 
-export default PhotosPage;
+export default connect(null,null)( PhotosPage);
