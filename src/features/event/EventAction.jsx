632897f3eb774e5e0afac0b9566bf98f6bc1763b
MAZHAR <mazharchdev@gmail.com>
2018-10-26 05:58:26 -0700

    Create event method

--- src/features/event/EventAction.jsx
+++ src/features/event/EventAction.jsx
@@ -1,16 +1,12 @@
-import {
-  CREATE_EVENT,
-  DELETE_EVENT,
-  UPDATE_EVENT,
-  FETCH_EVENTS
-} from "./EventConstant";
+import { DELETE_EVENT, UPDATE_EVENT, FETCH_EVENTS } from "./EventConstant";
 import {
   asyncActionStart,
   asyncActionFinish,
   asyncActionError
 } from "../async/asyncActions";
 import { fetchSampleData } from "../../app/data/mockApi";
-import { toastr } from "react-redux-toastr"
+import { toastr } from "react-redux-toastr";
+import { createNewEvent } from "../../app/common/util/helpers";
 export const fetchEvents = events => {
   return {
     type: FETCH_EVENTS,
@@ -18,15 +14,23 @@ export const fetchEvents = events => {
   };
 };
 export const createEvent = event => {
-  return async dispatch => {
+  return async (dispatch, getState, { getFirestore, getFirebase }) => {
     try {
-      dispatch({
-        type: CREATE_EVENT,
-        payload: { event }
+      const firestore = getFirestore();
+      const user = firestore.auth().currentUser;
+      const photoURL = getState().firebase.profile.photoURL;
+      const newEvent = createNewEvent(user, photoURL, event);
+      let createdEvent = await firestore.add(`events`, newEvent);
+      await firestore.set(`event_attendee/${createdEvent.id}_${user.uid}`, {
+        eventId: createdEvent.id,
+        userUid: user.uid,
+        eventDate: event.data,
+        host: true
       });
-      toastr.success("Success!","Event has been created")
+
+      toastr.success("Success!", "Event has been created");
     } catch (error) {
-      toastr.success("Oops","Something went wrong")
+      toastr.success("Oops", "Something went wrong");
     }
   };
 };
@@ -38,9 +42,9 @@ export const deleteEvent = eventid => {
         type: DELETE_EVENT,
         payload: { eventid }
       });
-      toastr.success("Success!","Event has been Deleted")
+      toastr.success("Success!", "Event has been Deleted");
     } catch (error) {
-      toastr.success("Oops","Something went wrong")
+      toastr.success("Oops", "Something went wrong");
     }
   };
 };
@@ -51,9 +55,9 @@ export const updateEvent = event => {
         type: UPDATE_EVENT,
         payload: { event }
       });
-      toastr.success("Success!","Event has been updated")
+      toastr.success("Success!", "Event has been updated");
     } catch (error) {
-      toastr.success("Oops","Something went wrong")
+      toastr.success("Oops", "Something went wrong");
     }
   };
 };
