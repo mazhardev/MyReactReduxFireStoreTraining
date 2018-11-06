4dc6a71a1a50e9450fb523554b7b349483300a35
MAZHAR <mazharchdev@gmail.com>
2018-11-06 12:08:24 -0800

    Updating the mapState function

--- src/features/event/eventDetailed/EventDetailedPage.jsx
+++ src/features/event/eventDetailed/EventDetailedPage.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import EventDetailedHeader from "./EventDetailedHeader";
 import EventDetailedInfo from "./EventDetailedInfo";
 import EventDetailedChat from "./EventDetailedChat";
@@ -7,20 +6,23 @@ import EventDetailedSidebar from "./EventDetailedSidebar";
 import { Grid } from "semantic-ui-react";
 import { connect } from "react-redux";
 import { withFirestore } from "react-redux-firebase";
-
-const mapState = (state, ownProps) => {
-  const eventId = ownProps.match.params.id;
+import { toastr } from "react-redux-toastr";
+const mapState = (state) => {
+ 
   let event = {};
-  if (eventId && state.events.length > 0) {
-    event = state.events.filter(event => event.id === eventId)[0];
+  if (state.firestore.ordered.events && state.firestore.ordered.events[0]) {
+    event = state.firestore.ordered.events[0];
   }
   return { event };
 };
 class EventDetailedPage extends Component {
   async componentDidMount() {
-    const { firestore, match } = this.props;
+    const { firestore, match,history } = this.props;
     let event = await firestore.get(`events/${match.params.id}`);
-    console.log(event);
+    if (!event.exists) {
+      history.push("/events");
+      toastr.error("Sorry", "Event not found!");
+    }
   }
 
   render() {
