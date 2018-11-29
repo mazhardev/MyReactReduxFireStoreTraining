89a4497a440890fa40b0e7eea0c0267cb3a6cba6
MAZHAR <mazharchdev@gmail.com>
2018-11-29 07:08:41 -0800

    Adding  tab functionality to change the query

--- src/features/user/UserDetailed/UserDetailedPage.jsx
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -29,8 +29,8 @@ const mapState = (state, ownProps) => {
     auth: state.firebase.auth,
     photos: state.firestore.ordered.photos,
     requesting: state.firestore.status.requesting,
-    events:state.events,
-    eventsLoading:state.async.loading
+    events: state.events,
+    eventsLoading: state.async.loading
   };
 };
 const actions = {
@@ -41,8 +41,19 @@ class UserDetailedPage extends Component {
     let events = await this.props.getUserEvents(this.props.userUid);
     console.log(events);
   }
+  changeTab = (e, data) => {
+    this.props.getUserEvents(this.props.userUid,data.activeIndex)
+  };
   render() {
-    const { profile, photos, auth, match, requesting,events,eventsLoading } = this.props;
+    const {
+      profile,
+      photos,
+      auth,
+      match,
+      requesting,
+      events,
+      eventsLoading
+    } = this.props;
     const isCurrentUser = auth.uid === match.params.id;
     const loading = Object.values(requesting).some(a => a === true);
     if (loading) return <LoadingComponent inverted={true} />;
@@ -52,7 +63,11 @@ class UserDetailedPage extends Component {
         <UserDetailedDescription profile={profile} />
         <UserDetailedSidebar isCurrentUser={isCurrentUser} />
         {photos && photos.length > 0 && <UserDetailedPhotos photos={photos} />}
-        <UserDetailedEvents events={events} eventsLoading={eventsLoading} />
+        <UserDetailedEvents
+          events={events}
+          eventsLoading={eventsLoading}
+          changeTab={this.changeTab}
+        />
       </Grid>
     );
   }
